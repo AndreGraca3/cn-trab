fe005ba619eeedcc1b1fc51403cb6b0232c7931a
André <sindrome.gracinha@gmail.com>
2024-04-06 17:12:44 +0100

    Fix dependency and started forum server.

--- lab2/forum/grpcForumServerApp/src/main/java/grpcserverapp/Service.java
+++ lab2/forum/grpcForumServerApp/src/main/java/grpcserverapp/Service.java
@@ -1,23 +1,40 @@
 package grpcserverapp;
 
+import forum.*;
 import io.grpc.Status;
 import io.grpc.StatusException;
 import io.grpc.stub.StreamObserver;
-import forum.ServiceGrpc;
-import java.util.Random;
 
-import static com.google.common.math.IntMath.isPrime;
+import java.util.Random;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 public class Service extends ServiceGrpc.ServiceImplBase {
-    public Service(int svcPort) {
 
+    // topic -> { map: username -> streamObserver }
+    ConcurrentMap<String, ConcurrentMap<String, StreamObserver<ForumMessage>>> topicSubscribers;
+
+    public Service(int svcPort) {
         System.out.println("Service is available on port:" + svcPort);
     }
 
     @Override
     public void isAlive(ProtoVoid request, StreamObserver<TextMessage> responseObserver) {
-        System.out.println("isAlive called!");
         responseObserver.onNext(TextMessage.newBuilder().setTxt("Service is alive").build());
         responseObserver.onCompleted();
+    }
+
+    @Override
+    public void topicSubscribe(SubscribeUnSubscribe request, StreamObserver<ForumMessage> responseObserver) {
+        String topic = request.getTopicName();
+        String username = request.getUsrName();
+        if (topic == null || username == null) {
+            responseObserver.onError(
+                    new StatusException(Status.INVALID_ARGUMENT.withDescription("Invalid request parameters"))
+            );
+            return;
+        }
+        topicSubscribers.computeIfAbsent(topic, k -> new ConcurrentHashMap<>())
+                .put(username, responseObserver);
     }
 }
