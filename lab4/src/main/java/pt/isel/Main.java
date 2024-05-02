30c0d4ad17741cb962dab72e07af7b97ae2be314
48459 - Diogo Santos <a48459@alunos.isel.pt>
2024-05-02 11:44:18 +0100

    Testing .gitignore :)

--- lab4/src/main/java/pt/isel/Main.java
+++ lab4/src/main/java/pt/isel/Main.java
@@ -1,7 +1,29 @@
 package pt.isel;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.FirestoreOptions;
+import pt.isel.firebase.FirebaseOperations;
+
 public class Main {
+
+    private static final String csvPath = "src/main/java/pt/isel/in/OcupacaoEspacosPublicos.csv";
+    private static final String collectionName = "OcupacaoTemporaria";
+    private static final String databaseId = "cn-geral-db";
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            GoogleCredentials credentials =
+                    GoogleCredentials.getApplicationDefault();
+
+            FirestoreOptions options = FirestoreOptions
+                    .newBuilder().setDatabaseId(databaseId).setCredentials(credentials)
+                    .build();
+            Firestore db = options.getService();
+
+            FirebaseOperations.insertDocuments(csvPath, db, collectionName);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
